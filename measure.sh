9f7a4a0fe03160d03c88b0d1e22bedb75b9ea59b
Gavin Morgan <gmorgan@euclidanalytics.com>
2017-04-27 17:21:51 -0700

    initial commit

---
+++ measure.sh
@@ -0,0 +1,12 @@
+#!/bin/sh -ex
+
+source ./git-log2json.sh
+source ./git-stat2json.sh
+
+pushd $1
+tmp=$(mktemp -d)
+stats > $tmp/stats.json
+logs > $tmp/logs.json
+popd
+
+node merge.js $tmp
---
+++ readme.md
@@ -0,0 +1,11 @@
+# RepoStats
+
+Produces a single JSON object representing net deletions, e.g.:
+
+```
+{
+  gmorgan: 100,
+  steve: -253627,
+  sbrady: 5,
+}
+```
